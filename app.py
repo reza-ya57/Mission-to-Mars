37893004f80d3cdcca6a3264e5b1e47ce36f3703
reza-ya57 <79761400+reza-ya57@users.noreply.github.com>
2021-05-12 01:02:51 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo 
-import scraping
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import Mission_to_Mars_Challenge
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URL"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -16,8 +16,8 @@ def index():
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update({}.mars_data, upsert=True)
+    mars_data = Mission_to_Mars_Challenge.scrape_all()
+    mars.update({},mars_data, upsert=True)
     return redirect('/', code=302)
 
 if __name__ == "__main__":
